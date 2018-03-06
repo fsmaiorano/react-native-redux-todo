ddf46b01643546091354f1319fef2b95831d9301
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-03-06 07:32:18 -0300

    add: API communication

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,9 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest } from 'redux-saga/effects';
+
+import { addFavoriteRquest } from './favorites';
 
 export default function* rootSaga() {
   return yield all([
-
+    takeLatest('ADD_FAVORITE_REQUEST', addFavoriteRquest),
   ]);
 }
